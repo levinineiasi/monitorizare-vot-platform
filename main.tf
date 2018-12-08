4352ca10c21d05a7133f6a28498a8c45ec9d70ac
Andrei Mustata <a.mustata@levi9.com>
2018-12-08 12:02:25 +0200

    Create SQL server

--- main.tf
+++ main.tf
@@ -12,3 +12,20 @@ resource "azurerm_resource_group" "voting_irregularities_rg" {
   name     = "${var.root_name}-rg"
   location = "${var.location}"
 }
+
+variable "sql_sa_login" {
+  type = "string"
+}
+
+variable "sql_sa_password" {
+  type = "string"
+}
+
+resource "azurerm_sql_server" "voting_irregularities_sql_server" {
+  name                         = "${var.root_name}-sql"
+  resource_group_name          = "${azurerm_resource_group.voting_irregularities_rg.name}"
+  location                     = "${azurerm_resource_group.voting_irregularities_rg.location}"
+  version                      = "12.0"
+  administrator_login          = "${var.sql_sa_login}"
+  administrator_login_password = "${var.sql_sa_password}"
+}
--- terraform.tfvars
+++ terraform.tfvars
@@ -1,2 +1,5 @@
 root_name = "voting-irregularities"
 location = "westeurope"
+
+sql_sa_login = "CHANGEME"
+sql_sa_password = "CHANGEME"
